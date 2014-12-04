509095eda8c9285f2e04637221057d486264336d
Allan Liang <itrance.al@gmail.com>
2014-12-03 19:15:19 -0700

    Added Favourite Users

--- fiplus-view/src/main/java/com/Fiplus/FavouriteUsersActivity.java
+++ fiplus-view/src/main/java/com/Fiplus/FavouriteUsersActivity.java
@@ -1,41 +1,50 @@
 package com.Fiplus;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import adapters.FavouriteUsersListAdapter;
+import model.FavouriteUsersListItem;
 
 /**
  * Created by Allan on 03/12/2014.
  */
-public class FavouriteUsersActivity extends MainScreenActivity {
-    /** @Override
+public class FavouriteUsersActivity extends Activity {
+
+    private ListView mFavouriteUsersList;
+    private FavouriteUsersListAdapter mFavouriteUsersListAdapter;
+
+    @Override
     protected void onCreate(Bundle savedInstancesState) {
         super.onCreate(savedInstancesState);
-        // TODO: Create fragment_favourite_users layout
-        setContentView(R.layout.fragment_favourite_users);
-    }
-
-
-    /**
-     * public class MyEventsActivity extends MainScreenActivity {
-    @Override
-    protected void onCreate(Bundle savedInstancesState) {
-    super.onCreate(savedInstancesState);
-    //setContentView(R.layout.fragment_my_events);
-
-    //PUT THIS WHEN CALLING A NEW INTENT
-    //finish();
-    //intent = new Intent(this, MyEventsActivity.class);
-    //startActivity(intent);
-
+        setContentView(R.layout.activity_favourite_users);
+        mFavouriteUsersList = (ListView) findViewById(R.id.favouriteUsersList);
+        setFavouriteUsersList();
     }
 
     @Override
-    protected void functionCheckInstance(Bundle savedInstancesState)
-    {
-    if (savedInstancesState == null)
-    {
-    // on first time display My Events fragment
-    getSupportFragmentManager().beginTransaction()
-    .replace(R.id.frame_container, FragmentMyEvents.newInstance(), FragmentMyEvents.TAG).commit();
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.activity_in_from_left, R.anim.activity_out_to_right);
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(R.anim.activity_in_from_left, R.anim.activity_out_to_right);
     }
+
+    private void setFavouriteUsersList() {
+        ArrayList<FavouriteUsersListItem> FavouriteUsersList = new ArrayList<FavouriteUsersListItem>();
+
+        FavouriteUsersList.add(new FavouriteUsersListItem("Bob", R.drawable.fiplus));
+        FavouriteUsersList.add(new FavouriteUsersListItem("Xerxes", R.drawable.fiplus));
+
+        mFavouriteUsersListAdapter = new FavouriteUsersListAdapter(this, FavouriteUsersList);
+        mFavouriteUsersList.setAdapter(mFavouriteUsersListAdapter);
+
     }
-     */
+
 }
