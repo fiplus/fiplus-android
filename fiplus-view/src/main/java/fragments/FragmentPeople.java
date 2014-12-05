c66b78ed1e7c9c492fea368713f7cc3be9e4ed1e
nevalera <nicolasvalera@hotmail.com>
2014-12-05 07:50:28 -0700

    Dummy configure profile and dummy recent users

--- fiplus-view/src/main/java/fragments/FragmentPeople.java
+++ fiplus-view/src/main/java/fragments/FragmentPeople.java
@@ -1,12 +1,22 @@
 package fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.Fiplus.R;
+import com.Fiplus.ViewProfileActivity;
+
+import java.util.ArrayList;
+
+import adapters.EventListAdapter;
+import adapters.FavouriteUsersListAdapter;
+import model.FavouriteUsersListItem;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +24,10 @@ import com.Fiplus.R;
  */
 public class FragmentPeople extends Fragment {
 
+    public static final String TAG = FragmentEvents.class.getSimpleName();
+
+    private ListView mPeopleList;
+    private FavouriteUsersListAdapter mFavoriteUserListAdapter ;
 
     public FragmentPeople() {
         // Required empty public constructor
@@ -24,8 +38,34 @@ public class FragmentPeople extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_generic_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_generic_list, container, false);
+        mPeopleList = (ListView) v.findViewById(R.id.eventsList);
+
+        mPeopleList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                //TODO: Add item click listener for view profile list
+                Intent intent = new Intent(getActivity().getBaseContext(), ViewProfileActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP);
+                startActivity(intent);
+            }
+        });
+
+        setPeopleList();
+
+        return v;
     }
 
 
+    private void setPeopleList() {
+        ArrayList<FavouriteUsersListItem> FavouriteUsersList = new ArrayList<FavouriteUsersListItem>();
+
+        FavouriteUsersList.add(new FavouriteUsersListItem("John Doe", R.drawable.fiplus));
+        FavouriteUsersList.add(new FavouriteUsersListItem("Xerxes", R.drawable.fiplus));
+
+        mFavoriteUserListAdapter = new FavouriteUsersListAdapter(getActivity(), FavouriteUsersList);
+        mPeopleList.setAdapter(mFavoriteUserListAdapter);
+
+    }
 }
