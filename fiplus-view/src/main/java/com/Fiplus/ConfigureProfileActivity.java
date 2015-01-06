ee4664598768e92ff9279484c689b22718f0363b
nevalera <nicolasvalera@hotmail.com>
2015-01-05 22:45:45 -0700

    Initial work for profile configuration

--- fiplus-view/src/main/java/com/Fiplus/ConfigureProfileActivity.java
+++ fiplus-view/src/main/java/com/Fiplus/ConfigureProfileActivity.java
@@ -1,6 +1,7 @@
 package com.Fiplus;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
@@ -13,9 +14,16 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.wordnik.client.api.UserfiApi;
+import com.wordnik.client.model.UserProfile;
+
+import org.json.JSONException;
+
 import java.util.ArrayList;
 
+import utils.IAppConstants;
 import utils.ListViewUtil;
+import utils.PrefUtil;
 
 
 public class ConfigureProfileActivity extends Activity {
@@ -39,20 +47,19 @@ public class ConfigureProfileActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configure_profile);
 
-        mImageView = (ImageView)findViewById(R.id.imageView);
+        mImageView = (ImageView) findViewById(R.id.imageView);
         mImageView.setImageResource(R.drawable.fiplus);
 
-        mProfileName = (EditText)findViewById(R.id.configure_profile_name);
-        mGender = (EditText)findViewById(R.id.configure_gender);
-        mAge = (EditText)findViewById(R.id.configure_age);
+        mProfileName = (EditText) findViewById(R.id.configure_profile_name);
+        mGender = (EditText) findViewById(R.id.configure_gender);
+        mAge = (EditText) findViewById(R.id.configure_age);
 
-        mInterestSpinner = (Spinner)findViewById(R.id.interests_spinner);
+        mInterestSpinner = (Spinner) findViewById(R.id.interests_spinner);
 
-        mInterestListView = (ListView)findViewById(R.id.interests_list);
+        mInterestListView = (ListView) findViewById(R.id.interests_list);
         mInterestListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 mInterestListItems.remove(position);
                 listAdapter.notifyDataSetChanged();
                 ListViewUtil.setListViewHeightBasedOnChildren(mInterestListView);
@@ -81,7 +88,7 @@ public class ConfigureProfileActivity extends Activity {
             }
         });
 
-        mAddTextView = (TextView)findViewById(R.id.add_interest_label);
+        mAddTextView = (TextView) findViewById(R.id.add_interest_label);
         setTitle(R.string.configure_profile);
 
         Spinner spinner = (Spinner) findViewById(R.id.interests_spinner);
@@ -104,14 +111,13 @@ public class ConfigureProfileActivity extends Activity {
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK ) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
             finish();
         }
         return super.onKeyDown(keyCode, event);
     }
 
-    public void onAddClick(View view)
-    {
+    public void onAddClick(View view) {
         mInterestListItems.add(mInterestSpinner.getSelectedItem().toString());
         listAdapter.notifyDataSetChanged();
         ListViewUtil.setListViewHeightBasedOnChildren(mInterestListView);
@@ -129,8 +135,8 @@ public class ConfigureProfileActivity extends Activity {
         overridePendingTransition(R.anim.activity_in_from_left, R.anim.activity_out_to_right);
     }
 
-    private void getProfile()
-    {
+    private void getProfile() {
+        //TODO: Create a GetProfileTask to populate the current user's profile when it works
         // Dummy profile
         mProfileName.setText("John Doe");
         mGender.setText("M");
@@ -141,4 +147,55 @@ public class ConfigureProfileActivity extends Activity {
         listAdapter.notifyDataSetChanged();
     }
 
+    class GetProfileTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            //TODO: Create the get profile object and get interests object, and then put the current user's information in here
+
+            try {
+                //TODO: Get the profile and interests here here when the stuff exists in the SDK
+            } catch (Exception e) {
+                return e.getMessage();
+            }
+
+
+            mProfileName.setText("Get this from the profile we got above");
+            mGender.setText("Get this from the profile we got above");
+            mAge.setText("Get this from the profile we got above");
+
+            mInterestListItems.add(0, "Get from interest above");
+            mInterestListItems.add(1, "Get from interests above");
+
+            return null;
+        }
+
+    }
+
+    class SaveProfileTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            UserfiApi userfiApi = new UserfiApi();
+            userfiApi.addHeader("X-DreamFactory-Application-Name", IAppConstants.APP_NAME);
+            userfiApi.setBasePath(IAppConstants.DSP_URL + IAppConstants.DSP_URL_SUFIX);
+
+            UserProfile userProfile = new UserProfile();
+            userProfile.setEmail(PrefUtil.getString(getApplicationContext(), IAppConstants.EMAIL, null));
+            userProfile.setAge(Integer.parseInt(mAge.getText().toString()));
+            userProfile.setGender(mGender.getText().toString());
+
+            try{
+                //TODO: Test this somehow cause there's no API method to get the profile yet
+                String response = userfiApi.put_profile(userProfile);
+                System.out.println(response.toString());
+            } catch (Exception e) {
+                return e.getMessage();
+            }
+
+            return null;
+        }
+
+    }
 }
