eaf27e3911e7a7482cfd08e219a9c6c032bd501b
Allan Liang <itrance.al@gmail.com>
2015-01-18 21:20:57 -0700

    Partial implementation of push notifications and googleplayservices

--- fiplus-view/src/main/java/com/Fiplus/MainScreenActivity.java
+++ fiplus-view/src/main/java/com/Fiplus/MainScreenActivity.java
@@ -1,13 +1,19 @@
 package com.Fiplus;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,6 +24,11 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
 import java.util.ArrayList;
 
 import adapters.NavDrawerListAdapter;
@@ -52,11 +63,39 @@ public class MainScreenActivity extends BaseFragmentActivity
     private ArrayList<NavDrawerItem> navDrawerItems;
     private NavDrawerListAdapter adapter;
 
+    // GCM items
+    GoogleCloudMessaging gcm;
+    String regid;
+    Context context;
+    // Google API Project ID
+    String SENDER_ID = "566736661694";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+
     @Override
     protected void onCreate(Bundle savedInstancesState)
     {
         super.onCreate(savedInstancesState);
         setContentView(R.layout.activity_mainscreen);
+
+        // Check device for Play Services APK. TODO: Check that GooglePlayServices is available on the device before calling methods
+        // that require it. Must be done for all onResume() and onCreate() methods for each Activity (Allan). If not available
+        // must disable features or prompt the user to download the latest GooglePlayServices
+
+        // TODO: Write checkPlayServices() method (Allan)
+        // Check device for Play Services APK. If check succeeds, proceed with GCM registration.
+        if (checkPlayServices()) {
+            gcm = GoogleCloudMessaging.getInstance(this);
+            regid = getRegistrationId(context);
+
+            if (regid.isEmpty()) {
+                registerInBackground();
+            }
+        } else {
+            Log.i(TAG, "No valid Google Play Services APK found.");
+        }
 
         /**
          * Create Event Button
@@ -287,4 +326,114 @@ public class MainScreenActivity extends BaseFragmentActivity
         getActionBar().setTitle(mTitle);
     }
 
+    /**
+     * Gets the current registration ID for application on GCM service.
+     * <p>
+     * If result is empty, the app needs to register.
+     * @return registration ID, or empty string if there is no existi registration ID.
+     */
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing regID is not guaranteed to work with the new
+        // app version.
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    /**
+     * @return Application's {@code SharedPreferences}.
+     */
+    private SharedPreferences getGCMPreferences(Context context) {
+        // This sample app persists the registration ID in shared preferences, but
+        // how you store the regID in your app is up to you.
+        // TODO: Change Activity name accordingly (Allan)
+        return getSharedPreferences(MainScreenActivity.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+
+    /**
+     * @return Application's version code from the {@code PackageManager}.
+     */
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
+    /**
+     * Registers the application with GCM servers asynchronously.
+     * <p>
+     * Stores the registration ID and app versionCode in the application's
+     * shared preferences.
+     * TODO: Implement (Allan)
+     */
+    private void registerInBackground() {
+
+    }
+
+        /**
+         * Sends the registration ID to your server over HTTP, so it can use GCM/HTTP
+         * or CCS to send messages to your app. Not needed for this demo since the
+         * device sends upstream messages to a server that echoes back the message
+         * using the 'from' address in the message.
+         * TODO: Finish implementation of server interaction (Allan)
+         */
+        private void sendRegistrationIdToBackend() {
+            // Your implementation here.
+        }
+
+    /**
+     * Stores the registration ID and app versionCode in the application's
+     * {@code SharedPreferences}.
+     * @param context application's context.
+     * @param regId registration ID
+     * TODO: Choose appropriate storage of Registration ID (Allan)
+     */
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    // TODO: Handle Registration Errors (Allan)
 }
