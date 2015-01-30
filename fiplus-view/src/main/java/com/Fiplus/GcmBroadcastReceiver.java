b893e5422c289c61fb6dffc7c7f5111dca8917a7
Allan Liang <itrance.al@gmail.com>
2015-01-30 10:33:18 -0700

    Partial push notifications. Testing regID from GCM

--- fiplus-view/src/main/java/com/Fiplus/GcmBroadcastReceiver.java
+++ fiplus-view/src/main/java/com/Fiplus/GcmBroadcastReceiver.java
@@ -2,6 +2,7 @@ package com.Fiplus;
 
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,29 +14,44 @@ public class GcmBroadcastReceiver extends BroadcastReceiver {
     public static final int NOTIFICATION_ID = 1;
     private NotificationManager mNotificationManager;
     NotificationCompat.Builder builder;
+    private Context savedContext;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        // TODO: Update push notification cases
+        sendNotification(intent.getDataString(), context);
 
     }
 
-    private void sendNotification(String msg) {
-        mNotificationManager = (NotificationManager)
-                this.getSystemService(Context.NOTIFICATION_SERVICE);
 
+    private void sendNotification(String msg, Context context) {
+        savedContext = context;
         NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(null)
-                        .setContentTitle("GCM Notification")
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(msg))
+                new NotificationCompat.Builder(savedContext)
+                        // TODO: Set icon (Allan)
+                        .setSmallIcon(0)
+                        .setContentTitle("Fi+")
                         .setContentText(msg);
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(savedContext, MainScreenActivity.class);
 
-        mBuilder.setContentIntent(contentIntent);
-        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+        // The stack builder object will contain an artificial back stack for the started Activity.
+        // This ensures that navigating backward from the Activity leads out of your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(savedContext);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainScreenActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) savedContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        // TODO: Change 1 to private int mId if required. mId allows you to update the notification later on. (Allan)
+        mNotificationManager.notify(1, mBuilder.build());
     }
-
-
 }
 
--- fiplus-view/src/main/java/com/Fiplus/MainScreenActivity.java
+++ fiplus-view/src/main/java/com/Fiplus/MainScreenActivity.java
@@ -43,8 +43,6 @@ import model.NavDrawerItem;
  */
 public class MainScreenActivity extends BaseFragmentActivity
 {
-    private static final String TAG = MainScreenActivity.class.getSimpleName();
-
     private float lastTranslate = 0.0f;
     private RelativeLayout mainScreenFrame;
     private Button mCreateEventButton;
@@ -65,17 +63,6 @@ public class MainScreenActivity extends BaseFragmentActivity
 
     private ArrayList<NavDrawerItem> navDrawerItems;
     private NavDrawerListAdapter adapter;
-
-    // GCM items
-    GoogleCloudMessaging gcm;
-    String regid;
-    Context context;
-    // Google API Project ID
-    String SENDER_ID = "566736661694";
-    public static final String PROPERTY_REG_ID = "registration_id";
-    private static final String PROPERTY_APP_VERSION = "appVersion";
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-
 
     @Override
     protected void onCreate(Bundle savedInstancesState)
@@ -327,137 +314,4 @@ public class MainScreenActivity extends BaseFragmentActivity
         mTitle = title;
         getActionBar().setTitle(mTitle);
     }
-
-    /**
-     * Gets the current registration ID for application on GCM service.
-     * If result is empty, the app needs to register.
-     * @return registration ID, or empty string if there is no existing registration ID.
-     */
-    private String getRegistrationId(Context context) {
-        final SharedPreferences prefs = getGCMPreferences(context);
-        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
-        if (registrationId.isEmpty()) {
-            Log.i(TAG, "Registration not found.");
-            return "";
-        }
-        // Check if app was updated; if so, it must clear the registration ID
-        // since the existing regID is not guaranteed to work with the new
-        // app version.
-        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-        int currentVersion = getAppVersion(context);
-        if (registeredVersion != currentVersion) {
-            Log.i(TAG, "App version changed.");
-            return "";
-        }
-        return registrationId;
-    }
-
-    /**
-     * @return Application's {@code SharedPreferences}.
-     */
-    private SharedPreferences getGCMPreferences(Context context) {
-        // App persists the registration ID in shared preferences
-        return getSharedPreferences(MainScreenActivity.class.getSimpleName(),
-                Context.MODE_PRIVATE);
-    }
-
-    /**
-     * @return Application's version code from the {@code PackageManager}.
-     */
-    private static int getAppVersion(Context context) {
-        try {
-            PackageInfo packageInfo = context.getPackageManager()
-                    .getPackageInfo(context.getPackageName(), 0);
-            return packageInfo.versionCode;
-        } catch (PackageManager.NameNotFoundException e) {
-            // should never happen
-            throw new RuntimeException("Could not get package name: " + e);
-        }
-    }
-
-    /**
-     * Registers the application with GCM servers asynchronously.
-     * Stores the registration ID and app versionCode in the application's shared preferences.
-     */
-    private void registerInBackground() {
-        class RegisterInBackgroundTask extends AsyncTask<Void, Void, String>{
-            @Override
-            protected String doInBackground(Void... params) {
-                String msg = "";
-                try {
-                    if (gcm == null) {
-                        gcm = GoogleCloudMessaging.getInstance(context);
-                    }
-                    regid = gcm.register(SENDER_ID);
-                    msg = "Device registered, registration ID=" + regid;
-                    sendRegistrationIdToBackend(regid);
-                    // Persist the regID - no need to register again.
-                    storeRegistrationId(context, regid);
-                } catch (IOException ex) {
-                    msg = "Error :" + ex.getMessage();
-                    // If there is an error, don't just keep trying to register.
-                    // Require the user to click a button again, or perform exponential back-off.
-                }
-                return msg;
-            }
-
-            @Override
-            protected void onPostExecute(String msg) {
-                Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-        /**
-         * Sends the registration ID to your server over HTTP, so it can use GCM/HTTP
-         * or CCS to send messages to your app. Not needed for this demo since the
-         * device sends upstream messages to a server that echoes back the message
-         * using the 'from' address in the message.
-         * TODO: Send regid to Loopback server application (Allan)
-         * You should send the registration ID to your server over HTTP,
-         * so it can use GCM/HTTP or CCS to send messages to your app.
-         * The request to your server should be authenticated if your app
-         * is using accounts.
-         */
-        private void sendRegistrationIdToBackend(String regid) {
-
-        }
-
-    /**
-     * Stores the registration ID and app versionCode in the application's
-     * {@code SharedPreferences}.
-     * @param context application's context.
-     * @param regId registration ID
-     */
-    private void storeRegistrationId(Context context, String regId) {
-        final SharedPreferences prefs = getGCMPreferences(context);
-        int appVersion = getAppVersion(context);
-        Log.i(TAG, "Saving regId on app version " + appVersion);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(PROPERTY_REG_ID, regId);
-        editor.putInt(PROPERTY_APP_VERSION, appVersion);
-        editor.commit();
-    }
-
-    /**
-     * Check the device to make sure it has the Google Play Services APK. If
-     * it doesn't, display a dialog that allows users to download the APK from
-     * the Google Play Store or enable it in the device's system settings.
-     */
-    private boolean checkPlayServices() {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
-                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            } else {
-                Log.i(TAG, "This device is not supported.");
-                finish();
-            }
-            return false;
-        }
-        return true;
-    }
-
-    // TODO: Handle Registration Errors (Allan)
 }
