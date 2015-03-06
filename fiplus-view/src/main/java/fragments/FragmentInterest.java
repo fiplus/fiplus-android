7e3b1a36362c922df8d45ef3965519d5d0e46d44
Nick <nicolasvalera887@gmail.com>
2015-03-06 09:22:57 -0700

    Interest tab

--- fiplus-view/src/main/java/fragments/FragmentInterest.java
+++ fiplus-view/src/main/java/fragments/FragmentInterest.java
@@ -1,6 +1,8 @@
 package fragments;
 
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,11 +12,20 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.Fiplus.R;
+import com.Fiplus.ViewEventActivity;
+import com.wordnik.client.api.MatchesApi;
+import com.wordnik.client.api.UsersApi;
+import com.wordnik.client.model.Activity;
+import com.wordnik.client.model.UserProfile;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import adapters.EventListAdapter;
 import model.EventListItem;
+import utils.IAppConstants;
+import utils.LocationUtil;
+import utils.PrefUtil;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -35,34 +46,93 @@ public class FragmentInterest extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_generic_list, container, false);
         mEventsList = (ListView) v.findViewById(R.id.eventsList);
-        setEventList();
         mEventsList.setOnItemClickListener(new EventItemClickListener());
 
         return v;
     }
 
     //TODO: Remove DUMMY EVENTS
-    private void setEventList()
+    private void setEventList(List<Activity> activities)
     {
-        ArrayList<EventListItem> eventList = new ArrayList<EventListItem>();
+        if (activities == null)
+            return;
+        ArrayList<EventListItem> eventList = new ArrayList<>();
 
-        //eventList.add(new EventListItem(R.drawable.ic_activities, "First Interest Event", "Calgary", "4:30PM", "10 Attendees"));
-        //eventList.add(new EventListItem(R.drawable.ic_configure, "Second Interest Event", "Calgary", "10:30PM", "11 Attendees"));
+        for(int i = 0; i < activities.size(); i++)
+            eventList.add(new EventListItem(
+                    R.drawable.ic_configure,
+                    activities.get(i).getName(),
+                    LocationUtil.getLocationStrings(activities.get(i).getLocations(), getActivity().getBaseContext()),
+                    activities.get(i).getTimes(),
+                    ((Integer)activities.get(i).getNum_attendees().intValue()).toString(),
+                    activities.get(i).getActivity_id()));
 
         mEventListAdapter = new EventListAdapter(getActivity(), eventList, TAG);
         mEventsList.setAdapter(mEventListAdapter);
 
+        mEventListAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        GetEvents getEvents = new GetEvents();
+        getEvents.execute();
     }
 
     protected class EventItemClickListener implements AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id)
         {
-            // TODO: Put implementation
+            String sEventID = mEventListAdapter.getItem(position).getEventId();
+            Intent intent = new Intent(getActivity(), ViewEventActivity.class);
+            intent.putExtra("eventID", sEventID);
+            startActivity(intent);
+        }
+    }
+
+    private class GetEvents extends AsyncTask<Void, Void, String>
+    {
+        protected List<Activity> response;
+
+        @Override
+        protected void onPreExecute()
+        {
+        }
+
+        @Override
+        protected String doInBackground(Void... params)
+        {
+            MatchesApi matchesApi = new MatchesApi();
+            matchesApi.addHeader("X-DreamFactory-Application-Name", IAppConstants.APP_NAME);
+            matchesApi.setBasePath(IAppConstants.DSP_URL + IAppConstants.DSP_URL_SUFIX);
+
+            UsersApi usersApi = new UsersApi();
+            usersApi.addHeader("X-DreamFactory-Application-Name", IAppConstants.APP_NAME);
+            usersApi.setBasePath(IAppConstants.DSP_URL + IAppConstants.DSP_URL_SUFIX);
+
+            try{
+                UserProfile profile = usersApi.getUserProfile(PrefUtil.getString(getActivity().getApplicationContext(), IAppConstants.USER_ID));
+                response = matchesApi.matchActivities(
+                        50.0,
+                        true,
+                        10.0,
+                        profile.getLocation());
+            } catch (Exception e) {
+                return e.getMessage();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            if (response != null)
+                setEventList(response);
         }
     }
 
