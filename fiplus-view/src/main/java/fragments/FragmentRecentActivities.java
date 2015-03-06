eb9b2d6eaf4b74e64a86014e99cbd270a1739325
“Jobelle <jsfirme@ucalgary.ca>
2015-03-06 16:33:18 -0700

    Removed the code for the dialog that pops up when an event list is empty. Added the code back for EventItemClickListener in case we want them to be able to see past events detail in the future.

--- fiplus-view/src/main/java/fragments/FragmentRecentActivities.java
+++ fiplus-view/src/main/java/fragments/FragmentRecentActivities.java
@@ -1,22 +1,15 @@
 package fragments;
 
-import android.app.DialogFragment;
-import android.app.FragmentManager;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.Fiplus.R;
-import com.Fiplus.ViewEventActivity;
-import com.astuetz.PagerSlidingTabStrip;
 import com.wordnik.client.api.UsersApi;
 import com.wordnik.client.model.Activity;
 
@@ -24,9 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import adapters.EventListAdapter;
-import adapters.RecentTabPagerAdapter;
 import model.EventListItem;
-import utils.AlertFragmentDialog;
 import utils.IAppConstants;
 import utils.LocationUtil;
 import utils.PrefUtil;
@@ -57,6 +48,7 @@ public class FragmentRecentActivities extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_generic_list, container, false);
         mEventsList = (ListView) v.findViewById(R.id.eventsList);
+        //mEventsList.setOnItemClickListener(new EventItemClickListener());
         GetRecentEvents getRecentEvents= new GetRecentEvents();
         getRecentEvents.execute();
         return v;
@@ -68,15 +60,8 @@ public class FragmentRecentActivities extends Fragment {
         {
             return;
         }
-        else if(activities.size() == 0)
-        {
-            FragmentManager fm = getActivity().getFragmentManager();
-            DialogFragment alertDialog = AlertFragmentDialog.newInstance("Be Fi+!");
-            alertDialog.setCancelable(false);
-            alertDialog.show(fm, "fragment_alert");
-        }
 
-        ArrayList<EventListItem> eventList = new ArrayList<>();
+        ArrayList<EventListItem> eventList = new ArrayList<EventListItem>();
 
         for(int i = 0; i < activities.size(); i++)
             eventList.add(new EventListItem(
@@ -92,6 +77,17 @@ public class FragmentRecentActivities extends Fragment {
         mEventListAdapter.notifyDataSetChanged();
 
     }
+
+//    protected class EventItemClickListener implements AdapterView.OnItemClickListener {
+//    @Override
+//     public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+//        {
+//            String sEventID = mEventListAdapter.getItem(position).getEventId();
+//            Intent intent = new Intent(getActivity(), ViewEventActivity.class);
+//            intent.putExtra("eventID", sEventID);
+//            startActivity(intent);
+//        }
+//    }
 
     private class GetRecentEvents extends AsyncTask<Void, Void, String>
     {
